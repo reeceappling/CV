bbc382d58fc8c6ba65ebdfa236794182adc5c786
reeceappling <reeceappling@gmail.com>
2026-03-10 15:51:10 -0400

    a little bit of cleanup

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -8,18 +8,12 @@ resource "cloudflare_dns_record" "acm_validation" {
   }
   zone_id = var.cloudflare_zone_id
   name = each.value.name
-  ttl     = 60
+  ttl     = 60 # Minimum value (seconds) for unproxied
   type    = each.value.type
-  comment = "Subdomain CNAME record for CV hosted on S3" # TODO: fix
-  # Use trimsuffix to remove the trailing dot that AWS sometimes adds, which Cloudflare doesn't require
-  # value   = trimsuffix(each.value.record, ".")
+  comment = "Certificate validation records for CV hosted on S3"
   content = trimsuffix(each.value.record, ".")
   proxied = false
-  # settings = { # TODO: del or reenable???
-  #   ipv4_only = true
-  #   ipv6_only = true
-  # }
-  # tags = ["owner:dns-team"]
+  tags = ["component:cv-site"]
 }
 
 resource "cloudflare_dns_record" "cv_subdomain_cname" {
@@ -27,13 +21,8 @@ resource "cloudflare_dns_record" "cv_subdomain_cname" {
   name = local.domain_name
   type    = "CNAME"
   comment = "Subdomain CNAME record for CV hosted on S3"
-  # TODO: value = aws_cloudfront_distribution.cloudfront_distribution.domain_name
   content = aws_cloudfront_distribution.s3_distribution.domain_name
   proxied = true
   ttl     = 1 # Must be 1 for proxied
-  # settings = { # TODO: del or reenable???
-  #   ipv4_only = true
-  #   ipv6_only = true
-  # }
-  # tags = ["owner:dns-team"]
+  tags = ["component:cv-site"]
 }
