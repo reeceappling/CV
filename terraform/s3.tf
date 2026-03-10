20b9c9460c4390933b54af4abf1a2ef66fe5fd4e
reeceappling <reeceappling@gmail.com>
2026-03-09 20:13:45 -0400

    actually I forgot something

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -64,7 +64,7 @@ resource "aws_s3_object" "website_files" {
   for_each = fileset(local.source_dir, "**")
   bucket = aws_s3_bucket.cv_site_bucket.id
   key    = each.value
-  source = "${local.source_dir}/${each.value}"
+  source = "${local.source_dir}${each.value}"
   acl = "public-read"
   etag   = filemd5("${local.source_dir}/${each.value}") # Etag ensures updates are detected
   content_type = lookup(local.mime_types, regex("\\.([^.]+)$", each.value)[0], "application/octet-stream")
