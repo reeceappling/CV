02e2781375304c6d72255e079e9282064a90b718
reeceappling <reeceappling@gmail.com>
2026-03-10 18:05:22 -0400

    try without headobject

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -39,8 +39,8 @@ resource "aws_s3_bucket_acl" "b_acl" {
 
 data "aws_iam_policy_document" "site_bucket" {
   statement {
-    actions   = ["s3:GetObject","s3:HeadObject"]
-    resources = ["${aws_s3_bucket.cv_site_bucket.arn}/*",aws_s3_bucket.cv_site_bucket.arn]
+    actions   = ["s3:GetObject"]
+    resources = ["${ aws_s3_bucket.cv_site_bucket.arn }", "${aws_s3_bucket.cv_site_bucket.arn}/*"]
 
     principals {
       type        = "Service"
