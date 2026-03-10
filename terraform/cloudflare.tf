2b32e42e5432cd63ca40c94c47719b9e2fd9e156
reeceappling <reeceappling@gmail.com>
2026-03-10 17:15:13 -0400

    try without cname record

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -16,13 +16,13 @@ resource "cloudflare_dns_record" "acm_validation" {
   # tags = ["component:cv-site"]
 }
 
-resource "cloudflare_dns_record" "cv_subdomain_cname" {
-  zone_id = var.cloudflare_zone_id
-  name = local.full_domain
-  type    = "CNAME"
-  comment = "Subdomain CNAME record for CV hosted on S3"
-  content = aws_cloudfront_distribution.s3_distribution.domain_name
-  proxied = true
-  ttl     = 1 # Must be 1 for proxied
-  # tags = ["component:cv-site"]
-}
+# resource "cloudflare_dns_record" "cv_subdomain_cname" { # TODO: reenable if needed
+#   zone_id = var.cloudflare_zone_id
+#   name = local.full_domain
+#   type    = "CNAME"
+#   comment = "Subdomain CNAME record for CV hosted on S3"
+#   content = aws_cloudfront_distribution.s3_distribution.domain_name
+#   proxied = true
+#   ttl     = 1 # Must be 1 for proxied
+#   # tags = ["component:cv-site"]
+# }
