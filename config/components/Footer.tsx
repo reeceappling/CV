0289693b5cea4866fb4bdd4fd3900efdb46314d6
reeceappling <reeceappling@gmail.com>
2026-03-11 17:58:13 -0400

    fix

--- config/components/Footer.tsx
+++ config/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
         return (
             <footer class={`${displayClass ?? ""}`}>
                 <p>
-                    © {year} Reece Appling
+                    © {year} Reece Appling. All Rights Reserved.
                 </p>
                 <ul>
                     {Object.entries(links).map(([text, link]) => (
