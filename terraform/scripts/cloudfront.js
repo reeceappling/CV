bab68c29e2a381d54cec75525dd61539bc4df7fe
reeceappling <reeceappling@gmail.com>
2026-03-13 17:09:50 -0400

    cloudfront function script comments

--- terraform/scripts/cloudfront.js
+++ terraform/scripts/cloudfront.js
@@ -2,9 +2,10 @@ function handler(event) {
     var request = event.request;
     var uri = request.uri;
 
-    // Check if URI lacks a file extension and does not end in /
+    // Check if URI lacks a file extension
     if (uri.length > 1) {
         if (!uri.includes('.')) {
+            // If directory, go to folder index page, otherwise just go to the named page
             if (uri.endsWith('/')) {
                 request.uri += 'index.html';
             } else {
