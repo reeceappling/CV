4394ba3a6caf064973a427734946e7c8aec42695
reeceappling <reeceappling@gmail.com>
2026-03-14 18:31:59 -0400

    disable dates at the top of pages

--- quartzModified/quartz/components/ContentMeta.tsx
+++ quartzModified/quartz/components/ContentMeta.tsx
@@ -29,9 +29,10 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     if (text) {
       const segments: (string | JSX.Element)[] = []
 
-      if (fileData.dates) {
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
-      }
+      // Disable dates at the top of pages
+      // if (fileData.dates) {
+      //   segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
+      // }
 
       // Display reading time if enabled
       if (options.showReadingTime) {
