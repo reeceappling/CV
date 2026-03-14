55cad5e0e6880d32a8d2d4804389b049a7e96ad1
reeceappling <reeceappling@gmail.com>
2026-03-14 18:21:06 -0400

    revert CICD

--- generator/subjectMatter.go
+++ generator/subjectMatter.go
@@ -11,7 +11,7 @@ type SubjectMatter string
 
 func (sm SubjectMatter) Link() string {
 	if string(sm) == "CI-CD" {
-		return linkFor("CI\\/CD", "cv", "subjectMatter", withoutSpaces(string(sm)))
+		return linkFor("CI-CD", "cv", "subjectMatter", withoutSpaces(string(sm))) // TODO; FIX SO IT SAYS CI/CD
 	}
 	return linkFor(string(sm), "cv", "subjectMatter", withoutSpaces(string(sm)))
 }
