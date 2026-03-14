da058513b15db77d40b6896e4dce91b5d79122c9
reeceappling <reeceappling@gmail.com>
2026-03-14 16:32:45 -0400

    fix another subject matter on a platform

--- generator/platform.go
+++ generator/platform.go
@@ -59,11 +59,13 @@ func setupPlatformSubjectMatters() {
 	NewPlatform("Gitlab").
 		WithSubjectMatters(smCiCd)
 	NewPlatform("Azure DevOps"). // TODO: USE
-					WithSubjectMatters(smDevOps)
+		WithSubjectMatters(smDevOps)
 	NewPlatform("Jira"). // TODO: USE
-				WithSubjectMatters(smDevOps)
+		WithSubjectMatters(smDevOps)
 	NewPlatform("Confluence"). // TODO: USE
-					WithSubjectMatters(smDocumentation)
+		WithSubjectMatters(smDocumentation)
 	NewPlatform("Slack"). // TODO: USE
-				WithSubjectMatters(smCommunication)
+		WithSubjectMatters(smCommunication)
+	NewPlatform("Small Improvements"). // TODO: USE
+		WithSubjectMatters(smObservability)
 }
