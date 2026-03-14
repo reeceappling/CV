881718d6fb669a84d6d62771d0131e5f4246c1b2
reeceappling <reeceappling@gmail.com>
2026-03-14 18:45:59 -0400

    More links in footer

--- quartzModified/quartz.layout.ts
+++ quartzModified/quartz.layout.ts
@@ -8,9 +8,13 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
+      "Home": "https://reece.appli.ng",
+      "CV": "https://reece.appli.ng/cv",
+      "Blog": "https://reece.appli.ng/Blog",
+      "My Links": "https://links.reece.appli.ng",
       GitHub: "https://github.com/reeceappling",
       "Linkedin": "https://linkedin.com/reeceappling",
-      "Other Links": "https://links.reece.appli.ng"
+      "Notes": "https://reece.appli.ng/Notes",
     },
   }),
 }
